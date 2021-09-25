c38433243af5d7cded5f597a8407794d9929d160
ElenaPrepodobnaja <73935522+ElenaPrepodobnaja@users.noreply.github.com>
2021-09-25 12:36:30 +0300

    hw-01

---
+++ README.md
@@ -0,0 +1,23 @@
+Запусти команды в терминале и сделай отдельный скриншот результата выполнения каждой команды.
+
+# Получаем и выводим весь список контактов в виде таблицы (console.table)
+node index.js --action list
+
+https://ibb.co/TL45bPf
+
+# Получаем контакт по id
+node index.js --action get --id 5
+
+https://ibb.co/x37CCf0
+
+# Добавялем контакт
+node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
+
+https://ibb.co/RbVXtyp
+
+# Удаляем контакт
+node index.js --action remove --id=3
+
+https://ibb.co/WBG8TPF
+
+
---
+++ contacts.js
@@ -0,0 +1,67 @@
+
+  const fs = require('fs/promises')
+  const path = require('path')
+  const crypto = require('crypto')
+
+  const contactsPath = path.join(__dirname, 'db', 'contacts.json');
+
+const readContacts = async () => {
+    try {
+        const result = await fs.readFile(contactsPath, 'utf8')
+        const contacts = JSON.parse(result);
+        return contacts;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function listContacts() {
+ return readContacts();
+};
+
+async function getContactById(contactId) {
+    try {
+        const contacts = await readContacts()
+        const [result] = contacts.filter((contact) => 
+        contact.id === Number(contactId))
+        return result;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function removeContact(contactId) {
+    try {
+        const contacts = await readContacts();
+        const newList = contacts.filter(contact => 
+            contact.id !== Number(contactId))
+        await fs.writeFile(contactsPath, JSON.stringify(newList), 'utf8');
+        return newList;
+    } catch (error) {
+        console.error(error); 
+    }
+}
+
+async function addContact(name, email, phone ) {
+    try {
+        const contacts = await readContacts();
+        const newContact = {
+            id: crypto.randomUUID(),
+            name,
+            email,
+            phone,
+        }
+        contacts.push(newContact);
+        await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2), );
+        return newContact;
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+module.exports = {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact,
+}
